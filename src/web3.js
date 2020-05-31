efd5887be44bb40cecf7d5b82c7cff493a087052
tobydickins <65092153+tobydickins@users.noreply.github.com>
2020-05-30 21:54:44 -0400

    Update web3.js

--- src/web3.js
+++ src/web3.js
@@ -2,6 +2,6 @@ import Web3 from 'web3';
 
 //const web3 = new Web3(window.web3.currentProvider);
 
-new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+const web3 = new Web3(web3.providers.HttpProvider('http://localhost:8545'));
 
 export default web3;
